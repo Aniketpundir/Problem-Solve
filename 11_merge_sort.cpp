7b9e15b753f866c4b9040bd75eea6ccb16b19add
Aniket Kumar <aniketpundir348@gmail.com>
2025-11-28 13:08:51 +0530

    Add problem

---
+++ 11_merge_sort.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+using namespace std;
+
+void merge(int arr[], int s, int e)
+{
+    int mid = (s + e) / 2;
+
+    int len1 = mid = s + 1;
+    int len2 = e - mid;
+
+    int *first = new int[len1];
+    int *second = new int[len2];
+
+    // copy vale
+    int k = s;
+
+    for (int i = 0; i < len1; i++)
+    {
+        first[i] = arr[k++];
+    }
+
+    k = mid + 1;
+    for (int i = 0; i < len2; i++)
+    {
+        second[i] = arr[k++];
+    }
+
+    // Merge both array
+
+    int index1 = 0;
+    int index2 = 0;
+    // main array index k
+    k = s;
+
+    while (index1 < len1 && index2 < len2)
+    {
+        if (first[index1] < second[index2])
+        {
+            arr[k++] = first[index1++];
+        }
+        else
+        {
+            arr[k++] = first[index2++];
+        }
+    }
+
+    while (index1 < len1)
+    {
+        arr[k++] = first[index1++];
+    }
+
+    while (index2 < len2)
+    {
+        arr[k++] = first[index2++];
+    }
+}
+
+void merge_sort(int arr[], int s, int e)
+{
+    // base case
+    if (s >= e)
+    {
+        return;
+    }
+
+    int mid = (s + e) / 2;
+
+    // left part sort karna hai
+    merge_sort(arr, s, mid);
+
+    // right part solve karna hai
+
+    merge_sort(arr, mid + 1, e);
+
+    // merge
+    merge(arr, s, e);
+}
+
+int main()
+{
+    int n;
+    cout << "Enter the array size: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter " << n << " elements in array:" << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    merge_sort(arr, 0, n - 1);
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
